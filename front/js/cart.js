e491cbd2d39e2772effe09e95408778bbaef861e
Thao <thao.maiphuong@outlook.fr>
2022-06-26 22:11:54 +0200

    Modify quantity of product in cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -20,17 +20,18 @@ if (cartItems.length === 0) {
   ).innerHTML = `Votre panier est vide, veuillez choisir les articles de la page <a href="../html/index.html"> Accueil</a>`;
 }
 //for each cartItem, get id and the rest
-cartItems.forEach((cartItem) => {
-  fetch(`http://localhost:3000/api/products/${cartItem._id}`)
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then((product) => {
-      const items = document.getElementById("cart__items");
-      //visual of product in cart
-      html += `<article class="cart__item" data-id="${product._id}" data-color="${cartItem.color}">
+const productsInCart = () => {
+  cartItems.forEach((cartItem) => {
+    fetch(`http://localhost:3000/api/products/${cartItem._id}`)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((product) => {
+        const items = document.getElementById("cart__items");
+        //visual of product in cart
+        html += `<article class="cart__item" data-id="${product._id}" data-color="${cartItem.color}">
       <div class="cart__item__img">
         <img src="${product.imageUrl}" alt="${product.altTxt}">
       </div>
@@ -51,17 +52,54 @@ cartItems.forEach((cartItem) => {
         </div>
       </div>
     </article>`;
-      items.innerHTML = html;
+        items.innerHTML = html;
+        //caculate totalQuantity and totalPrice
+        document.getElementById("totalQuantity").innerHTML = totalQuantity +=
+          cartItem.quantity;
+
+        document.getElementById("totalPrice").innerHTML = totalPrice +=
+          product.price * cartItem.quantity;
+      })
+      .catch((err) => {
+        // Une erreur est survenue
+        const items = document.getElementById("cart__items");
+        items.innerHTML = `Une erreur est survenu (${err})`;
+      });
+  });
+};
+productsInCart(cartItems);
+
+//.............Update & delete product from cart.............
+
+//modify quantity of product
+//Get all ItemQuantity element and set for each of them
+document.querySelectorAll(".itemQuantity").forEach((itemQty) => {
+    console.log("");
+  //listen to each of itemquanity
+  itemQty.addEventListener("change", (event) => {
+    console.log(itemQuantity);
+    event.preventDefault();
+    //find node that match :id cart__items
+    let changedQuantity = itemQty.closest(".cart__items");
+    //Doublecheck if product found in cart has same color & id with product of change
+    const productFound = cartItems.find(
+      (item) =>
+        item._id == changedQuantity.dataset._id && item.color == changedQuantity.dataset.color
+    );
+    if (productFound) {
+    
+      productFound.quantity = parseInt(itemQty.value);
+      localStorage.setItem("cart", JSON.stringify(cart));
       //caculate totalQuantity and totalPrice
       document.getElementById("totalQuantity").innerHTML = totalQuantity +=
         cartItem.quantity;
-
       document.getElementById("totalPrice").innerHTML = totalPrice +=
         product.price * cartItem.quantity;
-    })
-    .catch((err) => {
-      // Une erreur est survenue
-      const items = document.getElementById("cart__items");
-      items.innerHTML = `Une erreur est survenu (${err})`;
-    });
+    }
+  });
+});
+
+//delete cart
+document.querySelectorAll("deleteItem").forEach((item) => {
+  
 });
